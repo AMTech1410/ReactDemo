12986f61945af9ff516f89624543652139d43023
AMTech1410 <62494345+AMTech1410@users.noreply.github.com>
2020-03-25 17:31:12 +0530

    Create App.js
    App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './components/Main';
+
+const App = () => (
+
+  <div>
+    
+    <div className="container">
+      <Main />
+      </div>
+  </div>
+)
+
+export default App;
