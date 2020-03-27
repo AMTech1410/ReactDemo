f9b63d7d7024404236cbae9c9eade6352bc38fbe
AMTech1410 <62494345+AMTech1410@users.noreply.github.com>
2020-03-27 17:45:42 +0530

    Add files via upload
    Add js for make a backend API call

--- src/services/Path.js
+++ src/services/Path.js
@@ -1,1 +1,12 @@
+const publicPath = '/reactdemo/';
 
+export const Path = {
+  HOME: publicPath,
+  dashboard: `${ publicPath }dashboard/`,
+  products: `${ publicPath }products/`,
+  product: `${ publicPath }product`,
+  category: `${ publicPath }category`,
+  categoryList: `${ publicPath }categoryList/`,
+  logOut: `${ publicPath }logout/`,
+  
+};
